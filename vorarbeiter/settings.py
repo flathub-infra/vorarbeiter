3c881be50ffcc78e42e1c31ed56840075d1d8836
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-03-10 10:12:18 +0100

    Add support for DATABASE_URL

--- vorarbeiter/settings.py
+++ vorarbeiter/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +75,9 @@ WSGI_APPLICATION = 'vorarbeiter.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+default_db_url = f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=default_db_url)
 }
 
 
