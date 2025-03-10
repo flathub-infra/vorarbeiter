436c58f0af0f3ffde802688a6730cccc153c5d79
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-03-10 08:42:59 +0100

    Initial commit

---
+++ vorarbeiter/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vorarbeiter project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vorarbeiter.settings')
+
+application = get_wsgi_application()
