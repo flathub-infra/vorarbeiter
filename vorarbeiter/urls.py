d06ab7be4627f33ab2d5a8e33f01d69bc68e4cec
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-03-30 18:46:40 +0200

    Add callback API for job status updates from external systems

--- vorarbeiter/urls.py
+++ vorarbeiter/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/webhook/", include("webhooks.urls")),
+    path("api/", include("pipelines.urls")),
 ]
