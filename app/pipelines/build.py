f2b58f3b955b7253de918eb22500cd59643d9450
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-07-07 07:12:29 +0200

    Revert "Route ca. 50% of default builds to AWS"
    This reverts commit caadef2c54cffe0a4e129ce9590b390014ae48f6.

--- app/pipelines/build.py
+++ app/pipelines/build.py
@@ -1,4 +1,3 @@
-import random
 import secrets
 import uuid
 from datetime import datetime
@@ -118,8 +117,6 @@ class BuildPipeline:
 
             if pipeline.app_id in app_build_types:
                 build_type = app_build_types[pipeline.app_id]
-            elif random.random() < 0.5:
-                build_type = "medium"
             else:
                 build_type = pipeline.params.get("build_type", "default")
 
