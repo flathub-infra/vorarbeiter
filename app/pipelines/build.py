37545e5f83d82a54bdf1f7222037b4d168ffbe94
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-07-09 12:02:26 +0200

    Move MAME to medium runners

--- app/pipelines/build.py
+++ app/pipelines/build.py
@@ -39,7 +39,7 @@ app_build_types = {
     "org.telegram.desktop": "large",
     "org.gnome.Fractal": "large",
     "com.pot_app.pot": "large",
-    "org.mamedev.MAME": "large",
+    "org.mamedev.MAME": "medium",
 }
 
 
