1f23e066d21da3cf37ee3c8b821e41c9924b8245
bbhtt <bbhtt.zn0i8@slmail.me>
2026-02-21 14:14:11 +0530

    Turn off admin ping temporarily
    Prevent endless mention spam until infra is fixed

--- app/config.py
+++ app/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     statuspage_url: str = "https://status.flathub.org"
     sentry_dsn: str | None = None
     ff_reprocheck_issues: bool = False
-    ff_admin_ping_comment: bool = True
+    ff_admin_ping_comment: bool = False
 
     model_config = SettingsConfigDict(
         env_file=".env",
