2e15fb85d430f0e82ea3335c46cec9bdc667ffc5
Bart Piotrowski <b@bpiotrowski.pl>
2026-09-15 09:14:00 +0200

    Serve effective inactive repository list

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 from app.routes.dashboard import dashboard_router
 from app.routes.diffoscope import diffoscope_router
+from app.routes.inactive_repos import inactive_repos_router
 from app.routes.merge import merge_router
 from app.routes.pipelines import pipelines_router
 from app.routes.webhooks import webhooks_router
@@ -7,6 +8,7 @@ from app.routes.webhooks import webhooks_router
 __all__ = [
     "dashboard_router",
     "diffoscope_router",
+    "inactive_repos_router",
     "merge_router",
     "pipelines_router",
     "webhooks_router",
